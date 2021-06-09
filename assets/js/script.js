85c606a8155541cf0461ca1cefd39547f4a9d272
liam34 <indemandcoding@gmail.com>
2021-06-08 22:31:30 -0600

    14.1 CREATING OUR PLAYLIST

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,13 @@ var currentIndex = 0;
 var repeat = false;
 var shuffle = false;
 var userLoggedIn;
+var timer;
 
 function openPage(url) {
+
+    if(timer != null) {
+        clearTimeout(timer);
+    }
 
     if(url.indexOf("?") == -1) {
         url = url + "?";
@@ -19,6 +24,26 @@ function openPage(url) {
     $("#mainContent").load(encodedUrl);
     $("body").scrollTop(0);
     history.pushState(null, null, url);
+}
+
+function creatPlaylist() {
+    console.log(userLoggedIn);
+    var popup = prompt("please enter the name of your playlist");
+
+    if(popup != null) {
+        
+        $.post("includes/handlers/ajax/createPlaylist.php", { name: popup, username: userLoggedIn })
+        .done(function(error){
+
+            if(error != null) {
+                alert(error);
+                return;
+            }
+            //do something when ajax returns
+            openPage("yourMusic.php");
+        })
+
+    }
 }
 
 function formatTime(seconds) {
